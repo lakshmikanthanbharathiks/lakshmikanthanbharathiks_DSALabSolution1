99fc94c0d1fe83bfce6993eda7e8d8f161b56818
Lakshmi Kanthan Bharathi K S <101494062+lakshmikanthanbharathiks@users.noreply.github.com>
2022-05-03 00:23:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+package com.greatlearning.BalancedOrUnbalancedBrackets.Model;
+import java.util.*;
+public class Main 
+{
+	public static boolean checkForBalanced(String expr)
+    {
+        Deque<Character> parenthesisStack = new ArrayDeque<Character>();
+ 
+        for (int i = 0; i < expr.length(); i++)
+        {
+            char symbol = expr.charAt(i);
+ 
+            if (symbol == '(' || symbol == '[' || symbol == '{')
+            {
+                parenthesisStack.push(symbol);
+                continue;
+            }
+ 
+            if (parenthesisStack.isEmpty())
+                return false;
+            char checkSymbol;
+            switch (symbol) {
+            case ')':
+                checkSymbol = parenthesisStack.pop();
+                if (checkSymbol == '{' || checkSymbol == '[')
+                    return false;
+                break;
+ 
+            case '}':
+                checkSymbol = parenthesisStack.pop();
+                if (checkSymbol == '(' || checkSymbol == '[')
+                    return false;
+                break;
+ 
+            case ']':
+                checkSymbol = parenthesisStack.pop();
+                if (checkSymbol == '(' || checkSymbol == '{')
+                    return false;
+                break;
+            }
+        }
+        return (parenthesisStack.isEmpty());
+    }
+ 
+    // Driver code
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nPlease enter a string : ");
+        String expr = sc.nextLine();
+        if (checkForBalanced(expr))
+            System.out.println("\nThe entered String has Balanced Brackets");
+        else
+            System.out.println("\nThe entered Strings do not contain Balanced Brackets");
+    }
+}
